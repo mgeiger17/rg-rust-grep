122431463b4e7c4f5992828f4c098b1a02bf6090
Mattis Geiger <mattis.geiger@sap.com>
2026-04-30 19:48:37 +0200

    Added ignore-case option and fixed a issue for false len of marking the matching word (was one to long

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use clap::Parser;
 use colored::Colorize;
 use std::collections::{HashMap, LinkedList};
-use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -10,30 +10,42 @@ enum SearchResult {
     NotMatchedBecause(char),
 }
 
+#[derive(Parser, Debug)]
+#[command(version = "0.1.0", about = "Just a Grep alternative", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    searchable: String,
+    #[arg(short, long)]
+    filepath: String,
+    #[arg(short, long, default_value_t = false)]
+    ignore_case: bool,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Args = Args::parse();
 
-    // catch if there is something missing
-    if args.len() != 2 + 1 {
-        panic!("You need to Enter Query file!");
-    }
-    let searchable = &args[1];
-    let file_path = &args[2];
+    let searchable = &args.searchable;
+    let file_path = &args.filepath;
+    let ignore_case = args.ignore_case;
 
-    println!("\nQuery: {searchable} | File: {file_path}\n");
+    println!("\nQuery: `{searchable}` | File: `{file_path}`\n");
 
     let mut map: HashMap<char, usize> = HashMap::new();
 
     for (i, c) in searchable.chars().rev().enumerate() {
         if map.get(&c) == None {
-            map.insert(c, i);
+            if ignore_case {
+                map.insert(c.to_ascii_lowercase(), i);
+            } else {
+                map.insert(c, i);
+            }
         }
     }
 
     if let Ok(lines) = read_lines(file_path) {
         // Consumes the iterator, returns an (Optional) String
         for (i, line) in lines.map_while(Result::ok).enumerate() {
-            let result = search_searchable_in_string(&searchable, line, &map);
+            let result = search_searchable_in_string(&searchable, line, &map, ignore_case);
             println!("{}. {}", i + 1, result)
         }
     } else {
@@ -73,11 +85,16 @@ fn search_searchable_in_string(
     searchable: &String,
     string: String,
     steps_per_char: &HashMap<char, usize>,
+    ignore_case: bool,
 ) -> String {
-    let mut pivot = searchable.len();
+    let mut pivot = searchable.len() - 1;
     let mut matched_indicies: LinkedList<usize> = LinkedList::new();
+    let chars_string: Vec<char> = string.chars().collect();
     loop {
-        let char_at_pivot = string.chars().nth(pivot).unwrap();
+        let mut char_at_pivot = chars_string[pivot];
+        if ignore_case {
+            char_at_pivot = char_at_pivot.to_ascii_lowercase();
+        }
         let mut steps = steps_per_char
             .get(&char_at_pivot)
             .copied()
@@ -87,10 +104,11 @@ fn search_searchable_in_string(
             let start: isize = pivot as isize - searchable.len() as isize + 1;
             let end: usize = pivot + 1;
 
-            let result: SearchResult = check_word(&string[start as usize..end], searchable);
+            let result: SearchResult =
+                check_word(&string[start as usize..end], searchable, ignore_case);
             match result {
                 SearchResult::Matched() => {
-                    matched_indicies.push_back(pivot - searchable.len());
+                    matched_indicies.push_back(pivot + 1 - searchable.len());
                     steps = searchable.len();
                 }
                 SearchResult::NotMatchedBecause(char_to_proove) => {
@@ -112,7 +130,7 @@ fn search_searchable_in_string(
     let mut last = 0;
 
     for matched_at in matched_indicies {
-        let end = matched_at + searchable.len() + 1;
+        let end = matched_at + searchable.len();
         result_string.push_str(&string[last..matched_at]);
         result_string.push_str(&string[matched_at..end].green().to_string());
         last = end;
@@ -122,9 +140,15 @@ fn search_searchable_in_string(
     result_string
 }
 
-fn check_word(string: &str, searchable: &String) -> SearchResult {
+fn check_word(string: &str, searchable: &String, ignore_case: bool) -> SearchResult {
+    let searchable_chars: Vec<char> = searchable.chars().collect();
     for (index, char_to_proove) in string.chars().rev().enumerate() {
-        if !(char_to_proove == searchable.chars().rev().nth(index).unwrap()) {
+        let mut is_equal = char_to_proove == searchable.chars().rev().nth(index).unwrap();
+        if ignore_case {
+            is_equal = char_to_proove.to_ascii_lowercase()
+                == searchable_chars[searchable_chars.len() - 1 - index].to_ascii_lowercase()
+        }
+        if !(is_equal) {
             return SearchResult::NotMatchedBecause(char_to_proove);
         }
     }
@@ -134,6 +158,12 @@ fn check_word(string: &str, searchable: &String) -> SearchResult {
 // time spent: 40 min
 // lesson learned:
 // Rust hat keine Exception und nutzt Typen um zwischen Fehler zu unterscheiden
+// Rust hat ebenfalls keine Klassen oder Interfaces dafuer werden struct und traits genutzt
+// Rust hat das Ownerschip-Model mit 3 Regeln:
+//  1. Jeder Wert hat einen einzigen Owner
+//  2. Es kann nur einen Owner an einem Zeitpunkt geben
+//  3. Geht das Object, dass das Ownership besitzt out of scope, so wird der Speicherbereich
+//     geklaert
 // offene Frage:
 // Was sind Macros
 // Was machen die Pipes (|)
--- test.md
+++ test.md
@@ -1,3 +1,4 @@
 This is an example Text and simulation how the algorithm should work!
 This is an example example  exapleText and msimulation  example $how the algorithm should work!
 This is an example example  exapleText and msimulation  example $how the algorithm should! example
+LaLalalal was passiert hier dann lalala hallo halhala!
